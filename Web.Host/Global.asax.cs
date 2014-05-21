ea59feb5c571478b26ba9b6454db1b8848fc7fd7
Simon Pinn <simon@spinn.net.au>
2014-05-21 21:21:24 +1000

    basic pub sub

---
+++ Web.Host/Global.asax.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using NServiceBus;
+using NServiceBus.Features;
+
+namespace Web.Host
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            
+        }
+
+        private static IBus _bus;
+        public static IBus Bus
+        {
+            get
+            {
+                return _bus ?? (_bus = Configure.With().DefaultBuilder()
+                    .Log4Net()
+                    .UnicastBus()
+                    .CreateBus()
+                    .Start());
+            }
+        }
+
+    }
+}
---
+++ Web.Host/Handlers/ClientCreated.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Client.Domain.Messages.Events;
+using NServiceBus;
+
+namespace Web.Host.Handlers
+{
+    public class ClientCreated:IHandleMessages<IClientCreated>
+    {
+        public void Handle(IClientCreated message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
